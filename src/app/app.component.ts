aa4b4f58b0532719aaa000558c5af440da5777d8
hassounamohamed <mohamed.hassouna.2004.24@gmail.com>
2025-08-15 18:33:11 +0100

    contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { AnimatedBackgroundComponent } from "./animated-background/animated-background.component";
-import { HomeComponent } from "./home/home.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, NavbarComponent, AnimatedBackgroundComponent, HomeComponent],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent, AnimatedBackgroundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [MatFormFieldModule,MatInputModule,MatSelectModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
